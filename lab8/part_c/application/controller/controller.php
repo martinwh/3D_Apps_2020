78a56a5c58145c51fca81e74e77b49fd580c271f
martinwh <m.white@sussex.ac.uk>
2020-03-31 18:36:57 +0100

    3D Apps 2020 Lab 8 Final Codes

---
+++ lab8/part_c/application/controller/controller.php
@@ -0,0 +1,88 @@
+<?php
+include './debug/ChromePhp.php';
+ChromePhp::log('controller.php: Hello World');
+ChromePhp::log($_SERVER);	
+
+// Create the controller class for the MVC design pattern
+class Controller {
+
+	// Declare public variables for the controller class
+	public $load;
+	public $model;
+	
+	// Create functions for the controller class
+	function __construct($pageURI = null) // constructor of the class
+	{
+		$this->load = new Load(); 
+		$this->model = new Model();
+		// determine what page you are on
+		$this->$pageURI();
+	}
+    // home page function
+	function home()
+	{
+		$data = $this->model->model3D_info();
+		$this->load->view('view3DAppTest_2', $data);
+	}
+
+	function apiCreateTable()
+	{
+	  	// echo "Create table function";
+		$data = $this->model->dbCreateTable();
+		$this->load->view('viewMessage', $data);
+	}
+	function apiInsertData()
+	{
+		$data = $this->model->dbInsertData();
+	   	$this->load->view('viewMessage', $data);
+	}  
+	function apiGetData()
+	{
+		$data = $this->model->dbGetData();
+		$this->load->view('view3DAppData', $data);
+	}  
+
+	// New methods for Part C of Lab 7 Tutorial, which use AJAX
+	// Flickr API
+	function apiGetFlickrService()
+	{
+		$this->load->view('viewFlickrService');
+	}
+
+	// API call to read JSON data from a JSON file
+	function apiGetJson()
+	{
+		$this->load->view('viewJson');
+	}
+
+	// API call to select 3D images
+	function apiLoadImage()
+	{
+	   // Get the brand data from the (this) Model using the dbGetBrand() meyhod in this Model class	
+	   ChromePhp::warn('controller.php: [apiLoadImage] Get the Brand data');	
+	   $data = $this->model->dbGetBrand();
+	   // Note, the viewDrinks.php view being loaded here calls a new model
+	   // called modelDrinkDetails.php, which is not part of the Model class
+	   // It is a separate model illustrating that you can have many models
+	   ChromePhp::log($data);  
+	   $this->load->view('viewDrinks', $data);
+	}
+	
+	// Old test methods to test the MVC design pattern
+	function dbCreateTable()
+	{
+		echo "Create Table Function";
+	}
+
+	function dbInsertData()
+	{
+		echo "Data Insert Function";
+	}
+
+	function dbGetData()
+	{
+		echo "Data Read Function";
+	}
+
+}
+?>    
---
+++ lab8/part_c/application/model/phpDebug.php
@@ -0,0 +1,8 @@
+<?php
+		include '../../debug/ChromePhp.php';
+		ChromePhp::log('phpDebug.php: Hello World');
+        ChromePhp::log($_SERVER);	
+        ChromePhp::warn('phpDebug.php : Insert PHP Trace Error Mesasage');	
+
+
+?>
